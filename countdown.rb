7b76d17182a163188f7862a120ed00fb24c69e22
Brandon Williams <nis350ztt@gmail.com>
2020-07-10 06:00:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ midnight_countdown
 def midnight_countdown_with_sleep
     countdown_with_sleep = 20 
     while countdown_with_sleep > 0  
-    puts '#{countdown_with_sleep} SECOND(S)!'
+    puts "#{countdown_with_sleep} SECOND(S)!"
     countdown_with_sleep -= 1 
     sleep 1
   end
