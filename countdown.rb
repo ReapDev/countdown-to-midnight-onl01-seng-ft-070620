129e0dddd8f809d9a54d5a1a0a40474942036f55
Brandon Williams <nis350ztt@gmail.com>
2020-07-10 05:55:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
-
-def countdown
+def midnight_countdown
+    countdown = 20
+    while countdown >= 1 
+    puts("#{countdown} SECOND(S)!")
+    countdown -= 1 
+  end
+    puts "HAPPY NEW YEAR!"
 end
+
+midnight_countdown
+
+def midnight_countdown_with_sleep
+    countdown_with_sleep = 20
+    while countdown_with_sleep >= 1 
+    puts("#{countdown_with_sleep} SECOND(S)!")
+    countdown_with_sleep -= 1 
+    sleep 1
+  end
+    puts "HAPPY NEW YEAR!"
+end
+
+midnight_countdown_with_sleep
---
+++ test.rb
@@ -0,0 +1,35 @@
+  countdown = 20
+    while countdown > 0 
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1 
+    until countdown == 0
+    puts "HAPPY NEW YEAR!"
+  end
+
+  countdown_with_sleep = 20
+    while countdown_with_sleep > 0
+    puts "#{countdown_with_sleep} SECOND(S)!"
+    countdown_with_sleep -= 1 
+    sleep 1
+    until countdown_with_sleep == 0 
+    puts "HAPPY NEW YEAR!"
+  end
+  
+    countdown = 20
+    while countdown > 0 
+    countdown -= 1 
+    puts "#{countdown} SECOND(S)!"
+  end
+    until countdown == 0
+    puts "HAPPY NEW YEAR!"
+  end
+
+  countdown_with_sleep = 20
+    while countdown_with_sleep > 0
+    countdown_with_sleep -= 1 
+    puts "#{countdown_with_sleep} SECOND(S)!"
+    sleep 1
+  end
+    until countdown_with_sleep == 0 
+    puts "HAPPY NEW YEAR!"
+  end
